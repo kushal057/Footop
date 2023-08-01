63a4e1270df454cc80017cd3d0a5b29c99faaf04
Kushal Tiwari <tiwarikushal057@gmail.com>
2023-08-01 21:44:17 +0545

    bug: fix style attribute for color

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './Logo.module.css';
 
-const Logo = ({ text, color }) => {
+const Logo = ({ text, color="inherit" }) => {
   return (
-    <div className={styles.logo} style={{ color }}>
+    <div className={styles.logo} style={{ color: color }}>
       {text}
     </div>
   );
